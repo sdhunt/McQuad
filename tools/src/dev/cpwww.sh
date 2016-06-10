e5f422912656e034fd96d309da80b6191caf616e
Simon Hunt <simon@onlab.us>
2016-06-09 21:35:31 -0700

    Tagged version 1.2.0.

--- tools/src/dev/cpwww.sh
+++ tools/src/dev/cpwww.sh
@@ -3,8 +3,9 @@
 
 BACK=$PWD
 TARBALL=static.tar.gz
+VERSION=1.2.0
 
-cd ../../../web/target/mcquad-web-1.1.0-SNAPSHOT/
+cd ../../../web/target/mcquad-web-${VERSION}/
 echo BUILD TAR
 tar cvzf ../../$TARBALL --exclude META-INF --exclude WEB-INF .
 cd ../../../out/www
