ba638a99483174ab00c6deeb935ab5eea62720f6
Simon Hunt <simon@onlab.us>
2016-06-12 22:12:27 -0700

    Oops - fixed unit test for block color count.

--- core/src/test/java/com/meowster/mcquad/BlockColorsStoreTest.java
+++ core/src/test/java/com/meowster/mcquad/BlockColorsStoreTest.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertNull;
  */
 public class BlockColorsStoreTest extends AbstractTest {
 
-    private static final int EXP_NUM_BLOCK_RECORDS = 459;
+    private static final int EXP_NUM_BLOCK_RECORDS = 478;
 
     private BlockColorsStore store;
 
